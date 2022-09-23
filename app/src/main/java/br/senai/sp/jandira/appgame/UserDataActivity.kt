f2f6a0fc5159337136ac1ddf247d5fe7221c6775
Marina <98419843+MarinaSantello@users.noreply.github.com>
2022-09-23 11:21:03 -0300

    novo projeto de aplicativo para jogos

---
+++ app/src/main/java/br/senai/sp/jandira/appgame/UserDataActivity.kt
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.appgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_data)
+    }
+}
