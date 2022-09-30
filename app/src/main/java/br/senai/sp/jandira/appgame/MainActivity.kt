34365bebc958c3616a4560620255ef3fb425aaea
Marina <98419843+MarinaSantello@users.noreply.github.com>
2022-09-30 11:29:43 -0300

    mudancas nas views para desenvolvimento do projeto

--- app/src/main/java/br/senai/sp/jandira/appgame/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/appgame/MainActivity.kt
@@ -2,10 +2,36 @@ package br.senai.sp.jandira.appgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater = menuInflater
+
+        inflater.inflate(R.menu.menu_new_user, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.menu_save) {
+            Toast.makeText(this, "salvo", Toast.LENGTH_SHORT).show()
+            return true
+        } else if (item.itemId == R.id.menu_settings) {
+            Toast.makeText(this, "configurado", Toast.LENGTH_SHORT).show()
+            return true
+        } else {
+            Toast.makeText(this, "saiu", Toast.LENGTH_SHORT).show()
+            return true
+        }
     }
 }
--- app/src/main/java/br/senai/sp/jandira/appgame/UserDataActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/appgame/UserDataActivity.kt
@@ -2,10 +2,29 @@ package br.senai.sp.jandira.appgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class UserDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_data)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater = menuInflater
+
+        inflater.inflate(R.menu.menu_new_user, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        Toast.makeText(this, "clicou no menu", Toast.LENGTH_SHORT).show()
+
+        return true
     }
 }
