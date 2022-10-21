d68f67ec8703b079eef978f6e2f610294e157b1b
Marina <98419843+MarinaSantello@users.noreply.github.com>
2022-10-21 11:24:43 -0300

    atualizacoes e melhorias nas viwes do projeto

--- app/src/main/java/br/senai/sp/jandira/appgame/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/appgame/MainActivity.kt
@@ -1,15 +1,25 @@
 package br.senai.sp.jandira.appgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+//    lateinit var buttonEnter: Button // ***** findviewbyid
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        buttonEnter.setOnClickListener {
+//            val userData = Intent(this, UserDataActivity::class.java)
+//            startActivity(userData)
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
